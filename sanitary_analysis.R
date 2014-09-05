9cf63f0353ddf48c83fd7a27b82de0c102b8b5bf
Jason Miller <jmillerlaureate@gmail.com>
2014-09-05 13:00:11 -0400

    initial commit

---
+++ sanitary_analysis.R
@@ -0,0 +1,4 @@
+## File: sanitary_analysis.R
+## Description:The analysis
+## Copyright: (c) 2014
+## Jason D. Miller
---
+++ sanitary_functions.R
@@ -0,0 +1,15 @@
+## File: sanitary_functions.R
+## Description: Libraries + functions for the analysis
+## Copyright: (c) 2014
+## Jason D. Miller
+
+# install.packages("JSTORr")
+# install.packages("scholar")
+# install.packages("AER")
+require(JSTORr)
+require(scholar)
+require(tm)
+require(tm.plugin.webmining)
+require(stats)
+require(AER)
+require(stargazer)
